0ad87b91ec5e398a41f6979460738482136b6fd0
RiceCum1 <89083789+RiceCum1@users.noreply.github.com>
2021-11-06 00:03:53 -0400

    Create Silent.cpp

---
+++ Silent.cpp
@@ -0,0 +1,36 @@
+
+void(*GetViewPoint)(ULocalPlayer*, FMinimalViewInfo*, EStereoscopicPass) = nullptr;
+void GetViewPointHook(ULocalPlayer* this_LocalPlayer, FMinimalViewInfo* OutViewInfo, EStereoscopicPass StereoPass)
+{
+	GetViewPoint(this_LocalPlayer, OutViewInfo, StereoPass);
+
+	if (GetAsyncKeyState(hotkey)) {
+		OutViewInfo->Location = OriginalLocation;
+		OutViewInfo->Rotation = OriginalRotation;
+	}
+}
+
+void(*GetPlayerViewPoint)(APlayerController*, FVector*, FRotator*) = nullptr;
+void GetPlayerViewPointHook(APlayerController* this_PlayerController, FVector* Location, FRotator* Rotation) {
+
+	GetPlayerViewPoint(this_PlayerController, Location, Rotation);
+
+	OriginalLocation = *Location;
+	OriginalRotation = *Rotation;
+
+	if (GetAsyncKeyState(hotkey)) {
+		if (BestTarget) {
+			auto Player = BestTarget->Instigator;
+			if (Player) {
+				auto Mesh = Player->Mesh;
+				if (Mesh) {
+					// Get Target Bone Location
+					// Calculate Rotation
+					// Set Rotation
+					FRotator AimAt; // = TargetRotation
+					*Rotation = AimAt;
+				}
+			}
+		}
+	}
+}
