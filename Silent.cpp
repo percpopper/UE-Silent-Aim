e647e4b3ecc66662888f54a0945baf876a9b90c3
PERC POPPER <89083789+percpopper@users.noreply.github.com>
2023-01-21 07:09:40 -0800

    Update Silent.cpp

--- Silent.cpp
+++ Silent.cpp
@@ -7,7 +7,8 @@ void GetViewPointHook(ULocalPlayer* this_LocalPlayer, FMinimalViewInfo* OutViewI
 {
 	GetViewPoint(this_LocalPlayer, OutViewInfo, StereoPass);
 
-	if (GetAsyncKeyState(hotkey)) {
+	if (PlayerController->IsInputKeyDown(Key)) 
+	{
 		OutViewInfo->Location = OriginalLocation;
 		OutViewInfo->Rotation = OriginalRotation;
 	}
@@ -15,23 +16,23 @@ void GetViewPointHook(ULocalPlayer* this_LocalPlayer, FMinimalViewInfo* OutViewI
 
 // https://docs.unrealengine.com/4.26/en-US/API/Runtime/Engine/GameFramework/APlayerController/GetPlayerViewPoint/
 void(*GetPlayerViewPoint)(APlayerController*, FVector*, FRotator*) = nullptr;
-void GetPlayerViewPointHook(APlayerController* this_PlayerController, FVector* Location, FRotator* Rotation) {
-
+void GetPlayerViewPointHook(APlayerController* this_PlayerController, FVector* Location, FRotator* Rotation)
+{
 	GetPlayerViewPoint(this_PlayerController, Location, Rotation);
 
 	OriginalLocation = *Location;
 	OriginalRotation = *Rotation;
 
-	if (GetAsyncKeyState(hotkey)) {
-		if (BestTarget) {
-			auto Player = BestTarget->Instigator;
-			if (Player) {
-				auto Mesh = Player->Mesh;
-				if (Mesh) {
-					// Get Target Bone Location
-					FRotator TargetRotation; // = Calculate Rotation
-					*Rotation = TargetRotation; // Set Rotation
-				}
+	if (BestTarget)
+	{
+		if (PlayerController->IsInputKeyDown(Key))
+		{
+			USkeletalMeshComponent* Mesh = BestTarget->Mesh;
+			if (Mesh)
+			{
+				// Get Target Bone Location
+				FRotator TargetRotation; // = Calculate Rotation
+				*Rotation = TargetRotation; // Set Rotation
 			}
 		}
 	}
